d53321e4378c8fe92365258fb43f06c4bc504a60
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-11-20 23:25:54 +0100

    generate spices done

--- Store.cpp
+++ Store.cpp
@@ -5,8 +5,9 @@
 
 constexpr int marketSection = 4;
 
-Store::Store() { 
+Store::Store() {
     generateAlcos();
+    generateSpices();
 }
 
 int Store::getRand(int min, int max) const {
@@ -18,10 +19,12 @@ int Store::getRand(int min, int max) const {
 }
 
 void Store::generateAlcos() {
+    const std::vector<std::string> alcoNames {"Beer", "Vodka", "Rum", "Ale", "Absint", "Amarena"};
+    const std::vector<double> alcoContent { 7.5, 40.0, 50.0, 3.5, 75.0, 99.9};
     int i = 0;
     while (i < marketSection) {
-        auto alcoIndex = getRand(0, 5);
-        Alcohol alco(alcoNames[alcoIndex], getRand(10, 50), getRand(1, 5), alcoContent[alcoIndex]);
+        auto index = getRand(0, 5);
+        Alcohol alco(alcoNames[index], getRand(10, 50), getRand(1, 5), alcoContent[index]);
         if (std::none_of(stock_.begin(), stock_.end(), [&alco](const auto& ptr) { return ptr->getName() == alco.getName(); })) {
             stock_.emplace_back(std::make_unique<Alcohol>(alco));
             i++;
@@ -29,20 +32,25 @@ void Store::generateAlcos() {
     }
 }
 
+void STore::generateItems() {
+    
+}
+
 void Store::generateSpices() {
+    const std::vector<std::string> spiceNames{"red", "yellow", "black", "magenta", "cyan", "orange"};
     int i = 0;
     while (i < marketSection) {
-        auto spiceIndex = getRand(0, 5);
-        
-        if (std::none_of(stock_.begin(), stock_.end(), [](const auto& ptr) { return ptr->getName() == alco.getName(); })) {
-            stock_.emplace_back(std::make_unique<Spice>(alco));
+        auto index = getRand(0, 5);
+        Spice spice(spiceNames[index], getRand(250, 300), getRand(2, 20), getRand(5, 100));
+        if (std::none_of(stock_.begin(), stock_.end(), [&spice](const auto& ptr) { return ptr->getName() == spice.getName(); })) {
+            stock_.emplace_back(std::make_unique<Spice>(spice));
             i++;
         }
     }
 }
 
 void Store::showStore() const {
-    for (const auto& el :stock_) {
+    for (const auto& el : stock_) {
         std::cout << "Name: " << el->getName() << "   |   Amount: " << el->getAmount() << "   |   Price: " << el->getPrice() << "\n";
     }
 }
--- Store.hpp
+++ Store.hpp
@@ -7,28 +7,9 @@
 #include "Item.hpp"
 #include "Spice.hpp"
 
-const std::vector<std::string> alcoNames {
-    "Beer",
-    "Vodka",
-    "Rum",
-    "Ale",
-    "Absint",
-    "Amarena"
-};
-
-const std::vector<double> alcoContent {
-    7.5,
-    40.0,
-    40.0,
-    3.5,
-    75.0,
-    99.9
-};
-
 class Store {
 private:
     std::vector<std::unique_ptr<Cargo>> stock_{};
-
 
     int getRand(int min, int max) const;
 
