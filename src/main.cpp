db2843ca4c0f5b6a2be2f7d754aff564351630a4
iprice73 <mateusz.kowalski074@gmail.com>
2020-11-29 22:48:31 +0100

    main and useless include removed

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,6 @@
 #include "Store.hpp"
 #include "Player.hpp"
 
-std::string handleRespone(Response re) {
-    switch (re)
-    {
-    case Response::LackOfCargo:
-        return "We don't have this mush in stock.";
-    case Response::LackOfMoney:
-        return "You don't have money for this.";
-    case Response::LackOfSpace:
-        return "You don't have enough space.";
-    case Response::Done:
-        return "Transaction completed.";
-    default:
-        return "How did you get there?";
-    }
-}
-
 int main() {
     
     Ship ship;
@@ -32,15 +16,13 @@ int main() {
         std::cout << store;
         std::cout << "Index: " << " Amount: " << "      Your money: " << player.getMoney() << "    Avaiable space: " << ship.getAvaiableSpace() << '\n';
         std::cin >> index >> amount;
-        auto re = store.purchaseCargo(index - 1, amount, &player);
-        std ::cout << handleRespone(re) << '\n';
+        store.purchaseCargo(index - 1, amount, &player);
         std::cout << ship;
-        std::cout << store;
-        std::cout << "Index: " << " Amount: " << "      Your money: " << player.getMoney() << "    Avaiable space: " << ship.getAvaiableSpace() << '\n';
-        std::cin >> index >> amount;
-        auto ry = store.sellCargo(index - 1, amount, &player);
-        std::cout << ship;
-        std::cout << handleRespone(ry) << '\n';
+        // std::cout << store;
+        // std::cout << "Index: " << " Amount: " << "      Your money: " << player.getMoney() << "    Avaiable space: " << ship.getAvaiableSpace() << '\n';
+        // std::cin >> index >> amount;
+        // store.sellCargo(index - 1, amount, &player);
+        // std::cout << ship;
     }
 
     return 0;
