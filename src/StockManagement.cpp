3c8d563ec18aad1bd2cd0dc4198c6584fb915eb2
iprice73 <mateusz.kowalski074@gmail.com>
2020-12-07 18:38:02 +0100

    changed to push back

--- src/StockManagement.cpp
+++ src/StockManagement.cpp
@@ -25,7 +25,7 @@ std::unique_ptr<Cargo> StockManagement::makeNewCargo(const std::unique_ptr<Cargo
 void StockManagement::addCargo(std::unique_ptr<Cargo>&& cargo) {
     int amount = cargo->getAmount();
     if (auto existingCargoIt = std::find_if(stock_.begin(), stock_.end(), [&cargo](const auto& ptr) { return *ptr == *cargo; }); existingCargoIt == stock_.end()) {
-        stock_.emplace_back(std::move(cargo));
+        stock_.push_back(std::move(cargo));
     } else {
         **existingCargoIt += amount;
     }
