dd68899b7fd787233b1439fc9523ca842ce43eca
iprice73 <mateusz.kowalski074@gmail.com>
2021-01-08 22:21:01 +0100

    angle should be global

--- src/SolarSystem.cpp
+++ src/SolarSystem.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 
+
 const std::vector<std::pair<std::string, double>> planets{
     {"Mercury", 0.39},
     {"Venus", 0.723},
@@ -30,8 +31,9 @@ bool SolarSystem::travel(const Planet& dest) {
     return true;
 }
 
+double angle = 0;
+
 void SolarSystem::orbit(int days) {
-    double angle = 0;
     for (double i = 0; i < days; i++) {
         auto newX = static_cast<double>(map_[2].getDistance() * cos(angle));
         auto newY = static_cast<double>(map_[2].getDistance() * sin(angle));
