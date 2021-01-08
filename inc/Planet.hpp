478a93e2b029725bb486b6d6676a901d20b53703
iprice73 <mateusz.kowalski074@gmail.com>
2021-01-08 22:16:56 +0100

    setter ans getters for position, also store is now ptr

--- inc/Planet.hpp
+++ inc/Planet.hpp
@@ -6,14 +6,20 @@ class Planet {
 private:
     std::string name_{};
     double AU_{};
-    int posX_{};
-    int posY_{};
-    Store store_{};
+    double posX_{};
+    double posY_{};
+    Store* store_{};
 
 public:
-    Planet(const std::string& name = "Earth", double AU = 1.0, int posX = 1, int PosY = 1);
+    Planet(const std::string& name = "Earth", double AU = 1.0, double posX = 1, double PosY = 1);
     std::string getName() const { return name_; }
     double getDistance() const { return AU_; }
-    Store& getStore() { return store_; }
+    Store* getStore() { return store_; }
+
+    void setPos(double x, double y);
+
     bool operator==(const Planet& newPlanet);
+
+    double getX() const { return posX_; }
+    double getY() const { return posY_; }
 };
--- src/Planet.cpp
+++ src/Planet.cpp
@@ -1,8 +1,14 @@
 #include "Planet.hpp"
 
-Planet::Planet(const std::string& name, double AU, int posX, int posY)
+Planet::Planet(const std::string& name, double AU, double posX, double posY)
     : name_(name), AU_(AU), posX_(posX), posY_(posY) {}
+
+void Planet::setPos(double x, double y) {
+    posX_ = x;
+    posY_ = y;
+}
 
 bool Planet::operator==(const Planet& newPlanet) {
     return name_ == newPlanet.name_;
 }
+
