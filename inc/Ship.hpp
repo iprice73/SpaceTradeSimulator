6c0e7beaee69236157df0d0cf936e5eba97b157b
iprice73 <mateusz.kowalski074@gmail.com>
2021-04-05 22:54:11 +0200

    timr ptr in Ship and nextDay

--- inc/Ship.hpp
+++ inc/Ship.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "StockManagement.hpp"
+#include "Time.hpp"
 
 enum class EngineClass {
     ChemicalFuel = 5,
@@ -10,8 +11,9 @@ enum class EngineClass {
     ImpropabilityDrive = 9999
 };
 
-class Ship : public StockManagement {
+class Ship : public StockManagement, public Observer {
 private:
+    std::shared_ptr<Time> time_{};
     std::string name_{};
     int crewSize_{};
     int avaiableSpace_{};
@@ -20,16 +22,18 @@ private:
     void unload(const std::unique_ptr<Cargo>& cargo, int amount);
 
 public:
-    Ship(const std::string& name = "fajerwerka", int crewSize = 5, int avaiableSpace = 10, EngineClass engine = EngineClass::DarkMatter);
+    Ship(const std::shared_ptr<Time>& time, const std::string& name = "fajerwerka", int crewSize = 5, int avaiableSpace = 10, EngineClass engine = EngineClass::DarkMatter);
+    ~Ship();
 
     std::string getName() const { return name_; }
     int getCrew() const { return crewSize_; }
     int getAvaiableSpace() const { return avaiableSpace_; }
     EngineClass getEngine() const { return engine_; }
-    
+
     void load(std::unique_ptr<Cargo>&& cargo);
-    void changePrice(const std::unique_ptr<Cargo>& cargo); 
+    void changePrice(const std::unique_ptr<Cargo>& cargo);
 
     std::unique_ptr<Cargo> getCargo(size_t index, int amount);
 
+    void nextDay(int days) override;
 };
